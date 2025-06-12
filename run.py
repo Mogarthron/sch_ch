772c5cb2d664735add25abe82848186ab997dfee
Wit Łuczyński <witluczynski@gmail.com>
2025-06-12 21:38:55 +0200

    backend

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 from sqlalchemy import create_engine, func, cast, Float, Integer
 from sqlalchemy.orm import sessionmaker
-from models import Base, Oferta, Oferta_Szczegoly, Kategorie_Wyceny
+from models import Base, Oferta, Oferta_Szczegoly, Kategorie_Wyceny, Pozycje_Wyceny
 
 from datetime import datetime as dt, timedelta 
 
@@ -20,11 +20,64 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/pozycje_wyceny", methods=["GET", "POST"])
 def pozycje_wyceny():
 
-    return render_template("pozycje_wyceny.html")
+    pozycje = session.query(Pozycje_Wyceny).all()
+
+    return render_template("pozycje_wyceny.html", pozycje=pozycje)
+
+@app.route("/pobierz_nazwy_podkategorii", methods=["GET"])
+def pobierz_nazwy_podkategorii():
+    wyniki = session.query(Kategorie_Wyceny.pod_kategoria).all() #type: ignore
+    unikalne = [r[0] for r in wyniki]
+    return jsonify(unikalne)
+
+@app.route("/dodaj_pozycje_wyceny", methods=["POST"])
+def dodaj_pozycje_wyceny():
+    nazwa_kategorii = request.form.get("nazwa_kategorii")
+    id_kat_wyceny = int(session.query(Kategorie_Wyceny.katid).filter(Kategorie_Wyceny.pod_kategoria == nazwa_kategorii).first()[0]) #type: ignore
+    nazwa = request.form.get("nazwa_pozycji_wyceny")
+    cena_jednostkowa = float(request.form.get("cena_jednostkowa")) #type: ignore
+    jednostka_miary = request.form.get("jednostka_miary") 
+
+    nowa_pozycja = Pozycje_Wyceny(
+        kategoria_id=id_kat_wyceny,
+        pozycja=nazwa,
+        cena_jednostkowa=cena_jednostkowa,
+        jednostka_miary=jednostka_miary
+    )
+
+    try:
+        session.add(nowa_pozycja)
+        session.commit()
+        return redirect(url_for("dodaj_pozycje_wyceny"))
+    except Exception as e:
+        session.rollback()
+        return jsonify(success=False, error=str(e)), 500
+
+
+@app.route("/edytuj_pozycje_wyceny", methods=["POST"])
+def edytuj_pozycje_wyceny():
+    dane = request.get_json()
+    try:
+        poz = session.query(Pozycje_Wyceny).filter_by(cid=dane["cid"]).first()
+        if not poz:
+            return jsonify(success=False, error="Nie znaleziono pozycji")
+
+        poz.pozycja = dane["pozycja"]
+        poz.cena_jednostkowa = dane["cena_jednostkowa"]
+        poz.jednostka_miary = dane["jednostka_miary"]
+        session.commit()
+
+        return jsonify(success=True)
+    except Exception as e:
+        session.rollback()
+        return jsonify(success=False, error=str(e))
 
 
 @app.route("/kategorie_wyceny", methods=["GET", "POST"])
