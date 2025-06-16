d811b88f9484824cf949c30b349b238ff309d868
Wit Łuczyński <witluczynski@gmail.com>
2025-06-16 21:27:13 +0200

    backend

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+
+def create_app():
+    app = Flask(__name__)
+
+    UPLOAD_FOLDER = os.path.join("static", "zdjecia")
+    # ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+    app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+    
+
+
+
+
+    return app
