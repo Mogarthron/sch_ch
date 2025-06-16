bd47e1736ed67fe866183351c65c687e43e40733
Wit Łuczyński <witluczynski@gmail.com>
2025-06-16 21:28:14 +0200

    backend

---
+++ szczegoly_wyceny.py
@@ -0,0 +1,43 @@
+from flask import render_template, redirect, url_for, request, jsonify
+from models import session, Wycena, Pozycje_Wyceny, Kategorie_Wyceny, Szczegoly_Wyceny
+from run import app
+
+
+@app.route("/szczegoly_wyceny/<int:wycid>", methods=["GET","POST"])
+def szczegoly_wyceny(wycid):
+    wycena = session.query(Wycena).get(wycid)
+    szczegoly = session.query(Szczegoly_Wyceny).filter_by(wycid=wycid).all()
+    sekcje = Kategorie_Wyceny.dict_kat_podkat(session)
+    pozycje = session.query(Pozycje_Wyceny).all()
+
+
+    suma = sum(s.cena_calkowita for s in szczegoly)
+
+    if request.method == "POST":
+        szczegol_wyceny = Szczegoly_Wyceny(request.form, wycid, session)
+        session.add(szczegol_wyceny)
+        session.commit()
+
+        # print(request.form)
+
+        return redirect(url_for("szczegoly_wyceny", wycid=wycid))
+
+    return render_template("szczegoly_wyceny.html", 
+                            wycena=wycena,
+                            sekcje=sekcje,
+                            szczegoly=szczegoly,
+                            suma_wyceny=suma,
+                            # pozycje=pozycje
+                            )
+
+
+@app.route("/api/podkategorie_pozycje", methods=["GET", "POST"])
+def podkategorie_pozycje():
+    """
+    funkacja api która przekazuje pary podkategorie - pozycje do modala dodającego szczegół wyceny
+    """
+    podkategoria = request.args.get("podkategoria")
+    pozycje_podkategorii = session.query(Pozycje_Wyceny.cid, Pozycje_Wyceny.pozycja).join(Kategorie_Wyceny).filter(Kategorie_Wyceny.pod_kategoria == podkategoria).all()
+    print(podkategoria, pozycje_podkategorii)
+
+    return jsonify([{"id": p.cid, "nazwa": p.pozycja} for p in pozycje_podkategorii])
